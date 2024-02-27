50687c026b8fc17691c8e3723b780893a732569b
facundomoreno <facundoivanmoreno1@gmail.com>
2024-02-27 10:29:24 -0300

    ready for deploy

--- api/app.js
+++ api/app.js
@@ -11,17 +11,6 @@ var port = process.env.SERVER_PORT;
 const mongoString = process.env.DATABASE_URL;
 const minutesBetweenUpdates = process.env.MINUTES_BETWEEN_UPDATES;
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-
-database.on("error", (error) => {
-  console.log(error);
-});
-
-database.once("connected", () => {
-  console.log("Database Connected");
-});
-
 app.use(express.json());
 
 app.use("/api", routes);
@@ -30,14 +19,21 @@ app.get("/historical-prices", function (req, res) {
   res.send("Hello World!");
 });
 
-app.listen(port, function () {
-  console.log("App running in port: " + port);
-  updatePrices();
-  setInterval(async () => {
-    try {
-      await updatePrices();
-    } catch (e) {
-      console.log(e.message);
-    }
-  }, minutesBetweenUpdates * 60000);
+mongoose.connect(mongoString).then(() => {
+  const database = mongoose.connection;
+
+  database.on("error", (error) => {
+    console.log(error);
+  });
+  app.listen(port, function () {
+    console.log("App running in port: " + port);
+    updatePrices();
+    setInterval(async () => {
+      try {
+        await updatePrices();
+      } catch (e) {
+        console.log(e.message);
+      }
+    }, minutesBetweenUpdates * 60000);
+  });
 });
